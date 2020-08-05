dd90ac5914cc3fb48568b591fb283332340744e1
pukkamustard <pukkamustard@posteo.net>
2020-08-05 12:51:23 +0200

    crypto: use the npm packages blake2b and chacha20-universal instead of libsodium

--- src/crypto.js
+++ src/crypto.js
@@ -2,40 +2,73 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
-const sodium = require('libsodium-wrappers-sumo')
+const Blake2b = require('blake2b')
+var Chacha20 = require('chacha20-universal')
 
 const nullNonce = new Uint8Array(12)
 
 module.exports = {
 
   blake2b: async function (message, key) {
-    await sodium.ready
-    return sodium.crypto_generichash(32, message, key)
+    const hash = Blake2b(32, key)
+
+    hash.update(message)
+    var output = new Uint8Array(32)
+    hash.digest(output)
+
+    return output
   },
 
   chacha20: async function (input, key) {
-    await sodium.ready
-    return sodium.crypto_stream_chacha20_ietf_xor(input, nullNonce, key)
+    var xor = new Chacha20(nullNonce, key)
+
+    var output = new Uint8Array(input.length)
+    xor.update(output, input)
+
+    return output
   },
 
   pad: async function (buf, blockSize) {
-    await sodium.ready
-    return sodium.pad(buf, blockSize)
+    // number of blocks required
+    const n = Math.floor(buf.length / blockSize) + 1
+
+    var output = new Uint8Array(n * blockSize)
+
+    // place original data at start of output
+    output.set(buf)
+
+    // set the magic byte 0x80
+    output[buf.length] = 0x80
+
+    return output
   },
 
   unpad: async function (buf, blockSize) {
-    await sodium.ready
-    return sodium.unpad(buf, blockSize)
+    var n = buf.reduceRight((acc, byte, index) => {
+      if (acc) {
+        return acc
+      } else if (byte === 0) {
+        return acc
+      } else if (byte === 0x80) {
+        return index
+      }
+    }, false)
+
+    if (!n) {
+      throw new Error('invalid padding')
+    }
+
+    return buf.slice(0, n)
+  },
+
+  stream_xor: async function (input, nonce, key) {
   },
 
   is_zero: async function (buf) {
-    await sodium.ready
-    return sodium.is_zero(buf)
+    return buf.reduce((acc, byte) => (byte === 0) && acc, true)
   },
 
   memcmp: async function (a, b) {
-    await sodium.ready
-    return sodium.memcmp(a,b)
+    return (a.length === b.length) && a.reduce((acc, byte, index) => (byte === b[index]) && acc, true)
   }
-
 }
